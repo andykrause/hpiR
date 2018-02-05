73865b31a416076b38c631431a03ac07fb774930
akrause <andy@greenfieldadvisors.com>
2018-02-05 15:17:02 -0800

    Add: #16: Index volatility calculations

---
+++ R/calcIndexVolatility.R
@@ -0,0 +1,37 @@
+#' @title calcIndexVolatility
+#' @description Calculate index volatility
+#' @param index time-series index object
+#' @param window periods over which to calculate the volatility
+#' @param ... Additional arguments
+#' @return A metric of volatility
+#' @section Further Details:
+#' @examples
+#' index_vol <- calcIndexVolatility(index=index,
+#'                                  window=3)
+#' @export
+
+calcIndexVolatility <- function(index,
+                                window,
+                                ...){
+
+  # series: series of index values
+  # window:  length of time over which to measure the volatility
+
+  ## Calc series length
+
+  il <- length(index)
+
+  ## Calculate changes
+
+  deltas <- (index[-1] - index[-il]) / index[-il]
+
+  ## Calculate mean rolling sd
+
+  iv <- zoo::rollapply(deltas, window, sd)
+
+  ## Historic volatility
+
+  return(list(roll=iv,
+              mean=mean(iv)))
+
+}
