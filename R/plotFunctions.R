8672f4a7adacd9cac2fe99c7ee532634b7e2ca58
akrause <andy@greenfieldadvisors.com>
2018-02-05 10:45:18 -0800

    Add: Separate plotting methods for hpiindex and hpi classes.

--- R/plotFunctions.R
+++ R/plotFunctions.R
@@ -1,3 +1,33 @@
+#' @title plot.hpiindex
+#' @description Simple Plot of an hpiindex object
+#' @usage Lorem Ipsum...
+#' @param index_obj Object of class hpiindex
+#' @param ... Additional Arguments
+#' @return ggplot object
+#' @section Further Details:
+#' Lorem Ipsum...
+#' @examples
+#' a <- 1
+#' @export
+
+plot.hpiindex <- function(index_obj,
+                          ...){
+
+  ## Extract Data
+  hpi_data <- data.frame(x=index_obj$numeric,
+                         y=as.numeric(index_obj$index))
+
+  ## Make the base plot object
+  gg_obj <- ggplot(hpi_data, aes(x=x, y=y)) +
+    geom_line() +
+    ylab("Index") +
+    xlab('Time')
+
+  # Return Values
+  gg_obj
+
+}
+
 #' @title plot.hpi
 #' @description Simple Plot of an HPI object
 #' @usage Lorem Ipsum...
@@ -10,12 +40,15 @@
 #' a <- 1
 #' @export
 
-plot.hpiindex <- function(hpi_obj,
-                          ...){
+plot.hpi <- function(hpi_obj,
+                     ...){
+
+  # Extract index
+  index_obj <- hpi_obj$index
 
   ## Extract Data
-  hpi_data <- data.frame(x=rs_index$numeric,
-                         y=as.numeric(rs_index$index))
+  hpi_data <- data.frame(x=index_obj$numeric,
+                         y=as.numeric(index_obj$index))
 
   ## Make the base plot object
   gg_obj <- ggplot(hpi_data, aes(x=x, y=y)) +
