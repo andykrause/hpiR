95b1b1785e6f541f652733085be1ce73d941055f
akrause <andy@greenfieldadvisors.com>
2018-06-12 15:58:37 -0700

    Add: Weighted hedonic model option

--- R/hedModel.R
+++ R/hedModel.R
@@ -20,6 +20,29 @@ hedModel <- function(estimator,
                      hed_df,
                      hed_spec,
                      ...){
+
+  ## Check for proper classes
+
+  # hed_df object
+  if (!'hed' %in% class(hed_df)){
+    message('\nIncorrect class for "hed_df" object.  Must be of class "hed"')
+    return(NULL)
+  }
+
+  # Check that class is available
+  if (!paste0('hedModel.', class(estimator)) %in% methods(hedModel)){
+    message('\nInvalid estimator type: "', class(estimator), '" method not available.')
+    return(NULL)
+  }
+
+  # Check for sparseness
+  if (nrow(hed_df) < nrow(attr(hed_df, 'period_table'))){
+    message('\nYou have fewer observations (', nrow(hed_df), ') than number of periods (',
+            nrow(attr(hed_df, 'period_table')), '). Results will likely be unreliable.')
+  }
+
+  # Check for
+
   UseMethod("hedModel")
 
 }
@@ -38,6 +61,7 @@ hedModel <- function(estimator,
 #'                     time_matrix = time_matrix,
 #'                     price_diff = price_diff)
 #' @export
+
 ## For Base Estimator
 
 hedModel.base <- function(estimator,
@@ -45,10 +69,14 @@ hedModel.base <- function(estimator,
                           hed_spec,
                           ...){
 
-  hed_model <- lm(hed_spec, data=hed_df)
+  # Estimate model
+  hed_model <- lm(hed_spec,
+                  data=hed_df)
 
+  # Add class
   class(hed_model) <- 'hedmod'
 
+  # Return
   hed_model
 
 }
@@ -82,11 +110,49 @@ hedModel.robust <- function(estimator,
   if(time_size > 5){
     hed_model <- MASS::rlm(hed_spec, data=hed_df)
   } else {
-    hed_model <- robustbase::lmrob(hed_spec, data=hed_df)
+    hed_model <- robustbase::lmrob(hed_spec, data=hed_df, setting="KS2014")
   }
 
   class(hed_model) <- 'hedmod'
 
+  hed_model
+
+}
+
+#' @title hedModel.weighted
+#' @description Estimate hedonic model (method based on estimator class). Generic method.
+#' @param estimator Type of model to estimates (base, robust, weighted)
+#' @param hed_df Repeat sales dataset from hedCreateSales()
+#' @param hed_spec Model specfication
+#' @param ... Additional arguments
+#' @return hed model object
+#' @section Further Details:
+#' @export
+
+hedModel.weighted <- function(estimator,
+                              hed_df,
+                              hed_spec,
+                              ...){
+
+  # Extract weights
+  wgts <- list(...)$weights
+
+  if (length(wgts) != nrow(hed_df)){
+    wgts <- rep(1, nrow(hed_df))
+    message('You have supplied a set of weights that do not match the data.',
+            'Model being run in base OLS format.')
+  }
+
+  # Force environment
+  attr(hed_spec, '.Environment') <- environment()
+
+  # Estimate model
+  hed_model <- lm(as.formula(hed_spec), data=hed_df, weights=wgts)
+
+  # Add class
+  class(hed_model) <- 'hedmod'
+
+  # Return
   hed_model
 
 }
