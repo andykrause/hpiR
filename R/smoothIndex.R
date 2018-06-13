c213eb66931bc413d948c4b874e2c6f43f92f7b6
Andy Krause <andy@greenfieldadvisors.com>
2018-06-13 06:11:59 -0700

    Add: Initial functional upgrades to smoothing.

--- R/smoothIndex.R
+++ R/smoothIndex.R
@@ -13,28 +13,84 @@
 
 smoothIndex <- function(index,
                         order=3,
+                        in_place = FALSE,
+                        in_place_name = 'smoothed',
                         ...){
 
- s_index <- index
+  ## Save index_obj for future returning
+  index_obj <- index
 
- for(o.i in order){
-   s_index <- forecast::ma(x=s_index,
-                           order=o.i)
+  ## Strip from hpi or hpiindex objects
+  if ('hpi' %in% class(index_obj)){
+    index <- index$index$index
   }
 
- na_smooth <- which(is.na(s_index))
- na_low <- na_smooth[na_smooth < length(s_index)/2]
- s_index[na_low] <- index[na_low]
- na_high <- na_smooth[na_smooth > length(s_index)/2]
+  if ('hpiindex' %in% class(index_obj)){
+    index <- index$index
+  }
 
- high_fc <- forecast(ets(s_index[1:(na_high[1] - 1)], model='ANN'), h=length(na_high))
- new_high <- (high_fc$mean + index[na_high]) / 2
- s_index[na_high] <- new_high
+  ## Check for classes
+  if (!'ts' %in% class(index)){
+    message('The "index" object must be of class "ts"')
+    stop()
+  }
 
- class(s_index) <- append('smoothindex', class(index))
- attr(s_index, 'raw') <- index
+  # Check order
+  if (all(class(order) %in% c('numeric', 'integer')) && !is.na(order) &&
+      order > 0 && order <= length(index) / 2){
+    order <- as.integer(round(order, 0))
+  } else {
+    message('"order" argument must be a positive integer less than half the length of ',
+            'the index')
+    stop()
+  }
 
- s_index
+  # Create Smoothed index (retain existing)
+  s_index <- index
+
+  # Smooth with moving average (Multiple orders are possible; done sequentially)
+  for(o.i in order){
+    s_index <- forecast::ma(x=s_index,
+                            order=o.i)
+  }
+
+  # Deal with NAs (should be NAs on the tail ends of the smoothing)
+  na_smooth <- which(is.na(s_index))
+
+  # Fill in low end NAs with original index
+  na_low <- na_smooth[na_smooth < length(s_index) / 2]
+  s_index[na_low] <- index[na_low]
+
+  # Fill in High end NAs with forecasted values (off of smoothed)
+  na_high <- na_smooth[na_smooth >= length(s_index) / 2]
+  high_fc <- forecast(ets(s_index[1:(na_high[1] - 1)], model='ANN'), h=length(na_high))
+  new_high <- (high_fc$mean + index[na_high]) / 2
+  s_index[na_high] <- new_high
+
+  # Add Class and Attributes
+  class(s_index) <- append('smoothindex', class(index))
+  attr(s_index, 'raw') <- index
+  attr(s_index, 'order') <- order
+
+  ## Return
+
+  # If returing in place
+  if (in_place){
+
+    if ('hpi' %in% class(index_obj)){
+      index_obj$index$smoothed <- s_index
+      return(index_obj)
+    }
+
+    if ('hpiindex' %in% class(index_obj)){
+      index_obj$smoothed <- s_index
+      return(index_obj)
+    }
+  }
+
+  # If just returning result of volatility calculation
+
+  s_index
 
 }
 
