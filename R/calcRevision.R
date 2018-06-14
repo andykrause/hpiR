8d70da80b7e6634d18ca16aba794843061997b35
Andy Krause <andy@greenfieldadvisors.com>
2018-06-14 14:46:07 -0700

    Update: revision functions

--- R/calcRevision.R
+++ R/calcRevision.R
@@ -2,6 +2,9 @@
 #' @description Estimate revision figures for a series of indexes
 #' @usage Lorem Ipsum...
 #' @param index_obj A list of progressively longer indexes (from calcForecstErrors())
+#' @param series_name default = 'series'; name of the series if extracting
+#' @param in_place default = FALSE; calculating in place (adding to hpi)
+#' @param in_place_name default = 'rev'; name of revision object in_place
 #' @param ... Additional Arguments
 #' @return list containing:
 #' \item{period} Mean revision by period
@@ -11,11 +14,35 @@
 #' a <- 1
 #' @export
 
-calcRevision <- function(index_obj){
+calcRevision <- function(series_obj,
+                         series_name = 'series',
+                         in_place = FALSE,
+                         in_place_name = 'rev',
+                         ...){
+
+  # Extract if giving full hpi object
+  if ('hpi' %in% class(series_obj)){
+
+    if (!series_name %in% names(series_obj)){
+      message('The "hpi" object you provided does not have a "series" object ',
+              ' or the "series_name" you provided is incorrect')
+      stop()
+    }
+    series <- series_obj[[series_name]]
+  } else {
+    series <- series_obj
+  }
+
+  # Check class
+  if (is.null(series) || !'hpiseries' %in% class(series)){
+    message('"series_obj" must be of class "hpi_series" or a full "hpi" object with ',
+            ' a "series_obj" object')
+    stop()
+  }
 
   # Calculate the differences in the indexes (n to n+1)
-  index_diffs <- purrr::map(.x=2:length(index_obj),
-                            index_obj=index_obj,
+  index_diffs <- purrr::map(.x=2:length(series),
+                            index_obj=series,
                             .f=function(x, index_obj){
                               index_obj[[x]][-length(index_obj[[x]])] - index_obj[[x - 1]]
                              })
@@ -31,9 +58,16 @@ calcRevision <- function(index_obj){
   period_means <- unlist(purrr::map(.x=period_diffs,
                                     .f=mean))
 
-  # Return
-  structure(list(period = data.frame(period=1:length(period_means),
-                           revision=period_means),
-             total = mean(unlist(period_means))), class='indexrevision')
+  # Package and Return
+  rev_obj <- structure(list(period = data.frame(period=1:length(period_means),
+                                                revision=period_means),
+                            total = mean(unlist(period_means))),
+                       class='indexrevision')
 
+  if (in_place && 'hpi' %in% class(series_obj)){
+    series_obj[[in_place_name]] <- rev_obj
+    return(series_obj)
+  }
+
+  rev_obj
 }
