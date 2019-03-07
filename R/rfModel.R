b177705ea979d5b660817a397f3587a7a06fda4b
Andy Krause <andykrause19@gmail.com>
2019-03-06 17:44:30 -0800

    Update: Remove base estimator #49
    Remove base estimator from RF
    Fix small pred_grid error

--- R/rfModel.R
+++ R/rfModel.R
@@ -74,62 +74,6 @@ rfModel <- function(estimator,
 #' @section Further Details:
 #' See `?rfModel` for more information
 #' @inherit rfModel params
-#' @method rfModel base
-#' @importFrom ranger ranger
-#' @importFrom pdp partial
-#' @export
-
-rfModel.base <- function(estimator,
-                         rf_df,
-                         rf_spec,
-                         ntrees = 200,
-                         seed = 1,
-                         ...){
-
-  set.seed(seed)
-
-  # Estimate model
-  rf_model <- ranger::ranger(rf_spec,
-                             data = rf_df,
-                             num.tree = ntrees,
-                             seed = seed)
-
-  # Add class
-  class(rf_model) <- c('rfmodel', class(rf_model))
-
-  log_dep <- ifelse(grepl('log', rf_spec[2]), TRUE, FALSE)
-
-  pred_grid <- data.frame(trans_period = 1:max(attr(rf_df, 'period_table')$period))
-  pdp_df <- pdp::partial(rf_model,
-                         train = rf_df,
-                         pred.var = "trans_period",
-                         pred.grid = pred_grid)
-
-  if(log_dep){
-    coefs <- pdp_df$yhat - pdp_df$yhat[1]
-  } else {
-    coefs <- pdp_df$yhat / pdf_df$yhat[1]
-  }
-
-  rf_model$coefficients <- data.frame(time = 1:max(rf_df$trans_period),
-                                    coefficient = coefs)
-
-  rf_model
-  #
-  # rfSimulate(rf_obj = rf_model,
-  #            rf_df = rf_df,
-  #            log_dep = log_dep,
-  #            ...)
-}
-
-#'
-#' Hedonic model approach with base estimator
-#'
-#' Use of base estimator in hedonic model approach
-#'
-#' @section Further Details:
-#' See `?rfModel` for more information
-#' @inherit rfModel params
 #' @method rfModel pdp
 #' @importFrom ranger ranger
 #' @importFrom pdp partial
@@ -155,7 +99,7 @@ rfModel.pdp <- function(estimator,
 
   log_dep <- ifelse(grepl('log', rf_spec[2]), TRUE, FALSE)
 
-  pred_grid <- data.frame(trans_period = 1:max(attr(rf_df, 'period_table')$period))
+  pred_grid <- data.frame(trans_period = 1:max(rf_df$trans_period))
   pdp_df <- pdp::partial(rf_model,
                          train = rf_df,
                          pred.var = "trans_period",
